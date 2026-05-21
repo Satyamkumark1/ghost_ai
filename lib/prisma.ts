aa986c7cb2f9393fd75d096713f31d7819cc1164
Satyam <satyamnagar26@gmail.com>
2026-05-21 16:04:15 +0530

    fix(prisma): update generator provider and output path to resolve deployment issues

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../prisma/generated'
+import { PrismaClient } from '@/generated/prisma/client'
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { withAccelerate } from '@prisma/extension-accelerate'
